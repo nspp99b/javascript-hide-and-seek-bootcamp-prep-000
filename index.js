73bf1150e025973c0d4aa8908259d18936033ea6
Shahin Motia <chromepanthers@gmail.com>
2017-10-25 01:38:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 function getFirstSelector(selector) {
   return document.querySelector(`${selector}`)
 }
+
+//pulling .classes out of #targets
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
